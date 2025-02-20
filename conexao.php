34530310b8f72e89713086c60f6cdd8c1e03fbed
Julia Paiva <juliapoliver12@gmail.com>
2025-02-20 08:38:26 -0300

    conexão com o banco

--- conexao.php
+++ conexao.php
@@ -1,3 +1,14 @@
 <?php
 
+    $hostname = "localhost";
+    $bancodedados = "alphacode_bd";
+    $usuario = "root";
+    $senha = "";
+
+    $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+    if($mysqli->connect_errno){
+        echo "Falha ao conectar com o banco de dados mysql:(" . $mysqli->connect_errno . ")" . $mysqli->connect_errno;
+    }else
+     echo "Conectado ao banco de dados!!!";
+
 ?>
